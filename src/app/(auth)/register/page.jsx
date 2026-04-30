7d44a033784e694bc7807200917bb271f54bdf30
piasmajumdar <piasmajumdar@gmail.com>
2026-05-01 05:24:20 +0530

    Add authentication and main layout components, and create login and registration pages

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h2>Please Register</h2>
+        </div>
+    );
+};
+
+export default RegisterPage;
---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,10 @@
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            <h2>News Details Page</h2>
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
