a367922a03910e4b37750d193b376332302a9425
piasmajumdar <piasmajumdar@gmail.com>
2026-05-01 07:30:45 +0530

    Created custom Component- NavLink

---
+++ src/components/shared/NavLink.jsx
@@ -0,0 +1,15 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavLink = ({href, children, className}) => {
+    const pathName = usePathname();
+    const isActive = pathName === href;
+
+    return (
+        <Link href={href} className={`${isActive ? "border-b-2 border-b-purple-500": ""} ${className}`}>{children}</Link>
+    );
+};
+
+export default NavLink;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import userAvatar from "@/assets/user.png"
 import Image from 'next/image';
+import NavLink from './NavLink';
 
 const Navbar = () => {
     return (
@@ -9,15 +10,15 @@ const Navbar = () => {
             <div></div>
 
             <ul className='flex justify-between items-center gap-4 text-gray-700'>
-                <li><Link href={'/'}>Home</Link></li>
-                <li><Link href={'/about'}>About</Link></li>
-                <li><Link href={'/carrer'}>Career</Link></li>
+                <li><NavLink href={'/'}>Home</NavLink></li>
+                <li><NavLink href={'/about'}>About</NavLink></li>
+                <li><NavLink href={'/career'} className={"text-pink-700"}>Career</NavLink></li>
             </ul>
 
             <div className='flex items-center gap-2'>
                 <Image src={userAvatar} width={40} height={40} alt='User Avatar'></Image>
                 <button className='btn bg-purple-500 text-white'>
-                    <Link href={'/login'}>Login</Link>
+                    <NavLink href={'/login'}>Login</NavLink>
                 </button>
             </div>
         </div>
