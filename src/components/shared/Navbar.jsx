66d31d4b636ec3744f6e533f2293ea8b0ca7a38a
piasmajumdar <piasmajumdar@gmail.com>
2026-05-01 07:16:27 +0530

    Navbar created

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,25 @@
+import Link from 'next/link';
 import React from 'react';
+import userAvatar from "@/assets/user.png"
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className='w-11/12 mx-auto flex justify-between items-center gap-4 mt-6'>
+            <div></div>
+
+            <ul className='flex justify-between items-center gap-4 text-gray-700'>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/about'}>About</Link></li>
+                <li><Link href={'/carrer'}>Career</Link></li>
+            </ul>
+
+            <div className='flex items-center gap-2'>
+                <Image src={userAvatar} width={40} height={40} alt='User Avatar'></Image>
+                <button className='btn bg-purple-500 text-white'>
+                    <Link href={'/login'}>Login</Link>
+                </button>
+            </div>
         </div>
     );
 };
