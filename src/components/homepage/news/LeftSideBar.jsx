0dc18b66a14991662d4d2d131dc58063d9bba24d
piasmajumdar <piasmajumdar@gmail.com>
2026-05-01 09:57:54 +0530

    Left Side bar completed

--- src/components/homepage/news/LeftSideBar.jsx
+++ src/components/homepage/news/LeftSideBar.jsx
@@ -1,17 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const LeftSideBar = ({ categories, activeId }) => {
     return (
         <div className=" col-span-3">
             <h2 className="font-bold text-lg">All Categories</h2>
-            <ul className="flex flex-col gap-3 mt-6">
+            <ul className="flex flex-col gap-2 mt-6">
                 {
                     categories.news_category.map(category => {
                         return <li key={category.category_id}
                             className={`
                                 ${activeId === category.category_id && 'bg-slate-100 '}
                             p-2 rounded-md font-bold text-center text-md`}>
-                            {category.category_name}
+                            <Link href={`/category/${category.category_id}`} className='block p-2'>{category.category_name}</Link>
                         </li>
                     })
                 }
