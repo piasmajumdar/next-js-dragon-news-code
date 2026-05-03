24d798ce26d8194f7287d849638d7bdebba90ef8
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 10:04:26 +0530

    News Details fetch function Created

--- src/lib/data.js
+++ src/lib/data.js
@@ -9,3 +9,8 @@ export const getNewsCategoryId = async (category_id) => {
     const data = await res.json();
     return data.data;
 }
+export const getNewsDetailsById = async (news_id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${news_id}`);
+    const data = await res.json();
+    return data.data;
+}
