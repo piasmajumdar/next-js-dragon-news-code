b39e749df6d91f591c87fd69ac53a0d5303bfff5
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 12:13:52 +0530

    For several page, metadata

--- src/app/(main)/about/page.jsx
+++ src/app/(main)/about/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+  title: "Dragon News - About Us",
+  description: "Best News Portal in Bangladesh",
+};
+
 const AboutUsPage = () => {
     return (
         <div>
