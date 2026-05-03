74c3a15f49617f1a344e339eb4c274a3870a95bf
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 09:35:05 +0530

    loading spinner added

---
+++ src/app/(main)/loading.jsx
@@ -0,0 +1,10 @@
+const LoadingPage = () => {
+    return (
+        <div className='flex h-[60vh] justify-center items-center'>
+            Global Loading
+            <span className="loading loading-dots loading-xl"></span>
+        </div>
+    );
+};
+
+export default LoadingPage;
