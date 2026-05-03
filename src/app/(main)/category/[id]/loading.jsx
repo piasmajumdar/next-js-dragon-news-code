356eb79f7e32c2f562659043f50a5cf5c2e1e67c
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 09:37:17 +0530

    Category Page loading spinner added

---
+++ src/app/(main)/category/[id]/loading.jsx
@@ -0,0 +1,10 @@
+const LoadingPage = () => {
+    return (
+        <div className='flex h-[60vh] justify-center items-center'>
+            News By Category Loading
+            <span className="loading loading-dots loading-xl"></span>
+        </div>
+    );
+};
+
+export default LoadingPage;
