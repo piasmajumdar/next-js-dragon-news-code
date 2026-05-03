a29fc9ec3a4f746e0a92c985825ee09382e66e32
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 09:19:50 +0530

    News Card Design

--- src/components/homepage/news/NewsCard.jsx
+++ src/components/homepage/news/NewsCard.jsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
+import { CiBookmark, CiShare2 } from "react-icons/ci";
 
 const NewsCard = ({ news }) => {
     return (
         <div>
-            <div className="card bg-base-100 w-96 shadow-sm">
+            <div className="card bg-base-100 shadow-sm">
                 <div className="card-body">
                     {/* Author Details */}
-                    <div>
+                    <div className="flex justify-between items-center bg-slate-200 p-4">
                         <div>
                             <Image
                                 src={news.author?.img}
                                 alt={news.author?.name}
                                 height={40}
                                 width={40}
+                                className="rounded-full"
                             />
+                            <div>
+                                <h2 className="font-semibold">{news.author?.name}</h2>
+                                <h2 className="text-xs">{news.author?.published_date}</h2>
+                            </div>
                         </div>
-                        <div>
-
+                        <div className="flex justify-between items-center">
+                            <CiShare2 className="text-xl"></CiShare2>
+                            <CiBookmark className="text-xl"></CiBookmark>
                         </div>
                     </div>
                     <h2 className="card-title">{news.title}</h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <figure>
+                        <Image
+                            src={news.image_url}
+                            alt={news.title}
+                            height={300}
+                            width={300}
+                            className="w-full"
+                        />
+                    </figure>
+                    <p>{news.details}</p>
                 </div>
-                <figure>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
-                </figure>
             </div>
         </div>
     );
