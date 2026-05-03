00080fea8d5f3edbc8817c6980504afff4bb4310
piasmajumdar <piasmajumdar@gmail.com>
2026-05-03 10:07:51 +0530

    News Details fetch function Created 2

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,7 +1,11 @@
+import { getNewsDetailsById } from "@/lib/data";
 
-const NewsDetailsPage = async({params}) => {
-    const {id} = await params;
-    console.log(id);
+const NewsDetailsPage = async ({ params }) => {
+    const { id } = await params;
+    // console.log(id);
+    const newsDetails = await getNewsDetailsById(id);
+    console.log(newsDetails)
+
     return (
         <div>
             <h2>News Details Page</h2>
