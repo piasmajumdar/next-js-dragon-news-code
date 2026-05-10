683eeb1c9ff409f5ba79c0adae8c8304dcd02a61
piasmajumdar <piasmajumdar@gmail.com>
2026-05-10 23:36:05 +0530

    register page form

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-
 const RegisterPage = () => {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
@@ -13,50 +12,47 @@ const RegisterPage = () => {
     }
     // console.log(watch('email'))  // watch input value by passing the name of it
 
-    // console.log(errors, "errors")
+    console.log(errors, "errors")
 
     return (
         <div className='w-11/12 mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
 
             <div className='p-4 rounded-xl bg-white'>
-                <h2 className='font-bold text-center text-3xl mb-10'>Please Register your account</h2>
+                <h2 className='font-bold text-center text-3xl mb-10'>Login your account</h2>
 
                 <form onSubmit={handleSubmit(handleLoginFunc)} className='space-y-4'>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Name</legend>
                         <input type="text"
-                            className="input w-full"
+                            className="input"
                             placeholder="Type here Name"
-                            {...register("naem", { required: "Email is required" })}
+                            {...register("name", { required: "Name is required" })}
                         />
-                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Photo URL</legend>
+                        <input type="text"
+                            className="input"
+                            placeholder="Type here Photo URL"
+                            {...register("photo", { required: "Photo URL is required" })}
+                        />
+                        {errors.photo && <p className='text-red-500'>{errors.photo.message}</p>}
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Email</legend>
                         <input type="email"
-                            className="input w-full"
+                            className="input"
                             placeholder="Type here email"
                             {...register("email", { required: "Email is required" })}
                         />
-                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-
-                    </fieldset>
-                    <fieldset className="fieldset">
-                        <legend className="fieldset-legend">Email</legend>
-                        <input type="email"
-                            className="input w-full"
-                            placeholder="Type here email"
-                            {...register("email", { required: "Email is required" })}
-                        />
-                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-
+                        {errors.password && <p className='text-red-500'>{errors.email.message}</p>}
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Password</legend>
                         <input
                             type="password"
-                            className="input w-full"
+                            className="input"
                             placeholder="Type here Password"
                             {...register("password", { required: "Password field is required" })}
                         />
